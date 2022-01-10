f295955c7881f1801a2414c50c492d8b3f043754
talgatmendekov <talgatmendekov@gmail.com>
2022-01-11 01:35:38 +0600

    I add new object to expense array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+// import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
+
 
 function App() {
+
+  const expenses = [
+    {
+      id: 1, 
+      title:'House rent',
+      amount: 1000,
+      date: new Date(2020, 7, 14)
+    },
+    {
+      id: 2, 
+      title:'Car insuranse',
+      amount: 200,
+      date: new Date(2021, 6, 15)
+    },
+    {
+      id: 3, 
+      title:'Utilities',
+      amount: 400,
+      date: new Date(2021, 8, 20)
+    },
+    {
+      id: 4, 
+      title:'Food & Bevarage',
+      amount: 300,
+      date: new Date(2021, 9, 25)
+    },
+    {
+      id: 5, 
+      title:'Kindergarden',
+      amount: 300,
+      date: new Date(2021, 10, 26)
+    },
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Expenses data = {expenses}/>
     </div>
   );
 }
